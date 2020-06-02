0fff4a365cb2f863ce091033a100f22d8421be59
Rafnix Guzman <rafnixg@gmail.com>
2020-06-02 12:39:19 -0500

    [ADD] BSPWM, Polybar, SXHKD

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main -c $HOME/.config/polybar/config.ini &
+polybar bottom -c $HOME/.config/polybar/config.ini &
+# Lauch Bluetooth
+killall -q blueman-applet
+# blueman-applet &
